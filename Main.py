0b6c85da8aaefa32a655c171f3abeec62764e09c
Artem-Git-rgb <140831498+Artem-Git-rgb@users.noreply.github.com>
2023-11-10 18:05:43 +0300

    Создал проект

---
+++ Main.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import time
+from pygame.locals import (  # назначаю клавиши
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    KEYDOWN,
+    K_SPACE
+)
+class Game():
